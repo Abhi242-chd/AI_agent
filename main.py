ec6ce4919901a285503e32a2bccab6b5fabc4aa9
Abhi242-chd <abhishak2298@gmail.com>
2026-07-26 05:46:32 +0000

    CHI: LLMs L3: OpenRouter API

--- main.py
+++ main.py
@@ -16,8 +16,19 @@ def main():
     if api_key is None:
         raise RuntimeError("Missing api key")
 
-    print("Hello from ai-agent!")
+    client = OpenAI(
+            base_url="https://openrouter.ai/api/v1",
+            api_key=api_key,
+            )
 
+    response = client.chat.completions.create(
+            model = "openrouter/free",
+            messages = [ {
+        "role": "user",
+        "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    }
+])   
+    print(response.choices[0].message.content)
 
 if __name__ == "__main__":
     main()
