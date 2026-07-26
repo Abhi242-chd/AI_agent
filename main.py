5dbe51465e908734a6c8ef54b113fd098458b946
Abhi242-chd <abhishak2298@gmail.com>
2026-07-26 04:51:04 +0000

    openrouter work in progress

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
+
+
+
+
+
+
+
 def main():
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+    if api_key is None:
+        raise RuntimeError("Missing api key")
+
     print("Hello from ai-agent!")
 
 
