78b19a1134ba67d58bd4feb0dd2761ea3f719cba
Abhi242-chd <abhishak2298@gmail.com>
2026-08-25 06:44:10 +0000

    CH:1 LLMs L:5 user input

--- main.py
+++ main.py
@@ -17,23 +17,34 @@ def main():
     if api_key is None:
         raise RuntimeError("Missing api key")
 
+
+    # established connect with llm
     client = OpenAI(
             base_url="https://openrouter.ai/api/v1",
             api_key=api_key,
             )
 
+
+    # generate a user prompt 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
     messages = [ {
         "role": "user",
-        "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        "content": args.user_prompt,
     }
 ]
-
+    
+    # generatea llm responce
     response = client.chat.completions.create(
             model = "openrouter/free",
             messages = messages
             )   
+
     if response.usage is None:
         raise RuntimeError('No responce was generated')
+    
     print(f'User prompt: {messages[0]["content"]}') 
     print(f'Prompt tokens: {response.usage.prompt_tokens}') 
     print(f'Response tokens: {response.usage.completion_tokens}') 
