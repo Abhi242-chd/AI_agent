81c5e3e27bbd1584b93e16eca72c1d106879b69a
Abhi242-chd <abhishak2298@gmail.com>
2026-08-26 08:11:23 +0000

    CH:1 LLMs L:7 Verbose Output

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
-
+from openai.types.chat import ChatCompletion
 
 
 
@@ -25,9 +25,13 @@ def main():
             )
 
 
-    # generate a user prompt 
+    # parser generater
     parser = argparse.ArgumentParser(description="Chatbot")
+      # user prompt
     parser.add_argument("user_prompt", type=str, help="User prompt")
+      # verbose flag
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+
     args = parser.parse_args()
 
     messages = [ {
@@ -36,20 +40,37 @@ def main():
     },
 ]
     
-    # generatea llm responce
+    response = generate_response(messages, client) 
+
+    result(messages, response, args.verbose)
+    
+
+
+def generate_response(messages: list[dict[str, str]], client: OpenAI) -> ChatCompletion:
     response = client.chat.completions.create(
             model = "openrouter/free",
             messages = messages,
             )   
 
     if response.usage is None:
-        raise RuntimeError('No responce was generated')
+        raise RuntimeError('No response was generated')
     
-    print(f'User prompt: {messages[0]["content"]}') 
-    print(f'Prompt tokens: {response.usage.prompt_tokens}') 
-    print(f'Response tokens: {response.usage.completion_tokens}') 
-    print(f'Response:')
+    return response
+
+
+
+def result(messages: list[dict[str, str]], response: ChatCompletion, verbose: bool) -> None:
+    if verbose:
+       print(f'User prompt: {messages[0]["content"]}') 
+       print(f'Prompt tokens: {response.usage.prompt_tokens}') 
+       print(f'Response tokens: {response.usage.completion_tokens}') 
+    
+    print('Response:')
     print(f'{response.choices[0].message.content}')
+
+
+
+
 
 if __name__ == "__main__":
     main()
