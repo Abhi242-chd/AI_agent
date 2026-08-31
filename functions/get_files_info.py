d5f747d0f8a2bafc0d50af0acf7fb5a454bfd8ee
Abhi242-chd <abhishak2298@gmail.com>
2026-08-31 10:19:28 +0000

    CH2: Functions 3: List Files

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,16 +1,29 @@
 import os
+class Error(Exception):
+    pass
+
 
 def get_files_info(working_directory: str, directory: str = ".") -> str:
-    working_dir_abs = os.path.abspath(working_directory)
-    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
-    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(abs_working_dir, directory))
+    is_within_working_dir = os.path.commonpath([abs_working_dir, target_dir]) == abs_working_dir
+   
+
+    def format_directory_listing(entries: list) -> str:
+        def line_format(filename: str) -> str:
+            filepath = os.path.join(target_dir, filename)
+            return f' - {filename}: file_size={os.path.getsize(filepath)} bytes, is_dir={os.path.isdir(filepath)}'
+        
+        return "\n".join(map(line_format, entries))
+        
 
     try:
-        if not valid_target_dir:
-            raise Exception(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+        if not is_within_working_dir:
+            raise Error(f'Cannot list "{directory}" as it is outside the permitted working directory')
         elif not os.path.isdir(target_dir):
-            raise Exception(f'Error: "{directory}" is not a directory')
+            raise Error('"{directory}" is not a directory')
         elif os.path.isdir(target_dir):
-            return f'Success: "{directory}" is within the working directory'
-    except Exception as e:
-        return str(e)
+            return format_directory_listing(os.listdir(target_dir))
+    except Error as e:
+        return f'Error: {str(e)}'
+    
